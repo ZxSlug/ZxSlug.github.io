4136e7cb0366562ed49375a8376a89cf6953b13c
BuildTools <danieldandalii@gmail.com>
2023-09-14 18:41:15 +0300

    fix mobile

--- stuff.js
+++ stuff.js
@@ -39,7 +39,6 @@ function dragElement(elmnt) {
 
   function dragMouseDown(e) {
     e = e || window.event;
-    e.preventDefault();
     // get the mouse cursor position at startup:
     pos3 = e.clientX;
     pos4 = e.clientY;
@@ -48,12 +47,15 @@ function dragElement(elmnt) {
     
     // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
-    document.ontouchmove = elementDrag;
+    //document.ontouchmove = elementDrag;
+    document.addEventListener("ontouchmove", elementDrag, {passive: false});
+
+    //e.preventDefault();
   }
 
   function elementDrag(e) {
     e = e || window.event;
-    e.preventDefault();
+    //e.preventDefault();
     // calculate the new cursor position:
     pos1 = pos3 - e.clientX;
     pos2 = pos4 - e.clientY;
@@ -62,12 +64,14 @@ function dragElement(elmnt) {
     // set the element's new position:
     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+
+    e.preventDefault();
   }
 
   function closeDragElement() {
     // stop moving when mouse button is released:
     document.onmouseup = null;
-    document.ontouchstart = null;
+    document.ontouchend = null;
 
     document.onmousemove = null;
     document.ontouchmove = null;
