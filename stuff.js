e0ca15d936def9a9628d2688a68c6bfda5ab7af7
BuildTools <danieldandalii@gmail.com>
2023-12-02 22:55:39 +0200

    dumb stuff

--- stuff.js
+++ stuff.js
@@ -121,6 +121,8 @@ function dragElement(elmnt) {
 function tbarResize() {
     tbarh = $("#windowheader").height()
     $("#windowtitle").css("fontSize", `${tbarh/2.5}px`);
+    $(".taskbar-element").css("fontSize", `${tbarh/2.5}px`);
+    $(".taskbar-element span").css("height", `${tbarh/2.5+10}px`);
     $("#time").css("fontSize", `${tbarh/2.5}px`);
     $("#windowtitle").css("width", `${tbarh/0.03}px`);
     $("#windowheader img").css("height", `${tbarh*0.8}px`);
@@ -266,6 +268,121 @@ if (searchParams.has("old")) old();
 /* if (searchParams.has("rgb")) rgb(); */
 if (searchParams.has("no-tb")) $("#taskbar").remove();
 if (searchParams.has("creditless")) $("#credits").remove();
+
+
+let date = new Date()
+let currentTime = date.toLocaleString("en-US", {
+  hour12: true,
+  minute: "numeric",
+  hour: "numeric"
+})
+
+$("#time").text(currentTime)
+
+function christmas() {
+  $("body").append(`
+  <style>
+
+  /* customizable snowflake styling */
+  
+  .snowflake {
+  
+    color: #fff;
+  
+    font-size: 2em;
+  
+    font-family: Arial, sans-serif;
+  
+    text-shadow: 0 0 5px #000;
+  
+  }
+  
+   
+  
+  .snowflake,.snowflake .inner{animation-iteration-count:infinite;animation-play-state:running}@keyframes snowflakes-fall{0%{transform:translateY(0)}100%{transform:translateY(110vh)}}@keyframes snowflakes-shake{0%,100%{transform:translateX(0)}50%{transform:translateX(80px)}}.snowflake{position:fixed;top:-10%;z-index:9999;-webkit-user-select:none;user-select:none;cursor:default;animation-name:snowflakes-shake;animation-duration:3s;animation-timing-function:ease-in-out}.snowflake .inner{animation-duration:10s;animation-name:snowflakes-fall;animation-timing-function:linear}.snowflake:nth-of-type(0){left:1%;animation-delay:0s}.snowflake:nth-of-type(0) .inner{animation-delay:0s}.snowflake:first-of-type{left:10%;animation-delay:1s}.snowflake:first-of-type .inner,.snowflake:nth-of-type(8) .inner{animation-delay:1s}.snowflake:nth-of-type(2){left:20%;animation-delay:.5s}.snowflake:nth-of-type(2) .inner,.snowflake:nth-of-type(6) .inner{animation-delay:6s}.snowflake:nth-of-type(3){left:30%;animation-delay:2s}.snowflake:nth-of-type(11) .inner,.snowflake:nth-of-type(3) .inner{animation-delay:4s}.snowflake:nth-of-type(4){left:40%;animation-delay:2s}.snowflake:nth-of-type(10) .inner,.snowflake:nth-of-type(4) .inner{animation-delay:2s}.snowflake:nth-of-type(5){left:50%;animation-delay:3s}.snowflake:nth-of-type(5) .inner{animation-delay:8s}.snowflake:nth-of-type(6){left:60%;animation-delay:2s}.snowflake:nth-of-type(7){left:70%;animation-delay:1s}.snowflake:nth-of-type(7) .inner{animation-delay:2.5s}.snowflake:nth-of-type(8){left:80%;animation-delay:0s}.snowflake:nth-of-type(9){left:90%;animation-delay:1.5s}.snowflake:nth-of-type(9) .inner{animation-delay:3s}.snowflake:nth-of-type(10){left:25%;animation-delay:0s}.snowflake:nth-of-type(11){left:65%;animation-delay:2.5s}
+  
+  </style>
+  
+  <div class="snowflakes" aria-hidden="true">
+  
+    <div class="snowflake">
+  
+      <div class="inner">❅</div>
+  
+    </div>
+  
+    <div class="snowflake">
+  
+      <div class="inner">❅</div>
+  
+    </div>
+  
+    <div class="snowflake">
+  
+      <div class="inner">❅</div>
+  
+    </div>
+  
+    <div class="snowflake">
+  
+      <div class="inner">❅</div>
+  
+    </div>
+  
+    <div class="snowflake">
+  
+      <div class="inner">❅</div>
+  
+    </div>
+  
+    <div class="snowflake">
+  
+      <div class="inner">❅</div>
+  
+    </div>
+  
+    <div class="snowflake">
+  
+      <div class="inner">❅</div>
+  
+    </div>
+  
+    <div class="snowflake">
+  
+      <div class="inner">❅</div>
+  
+    </div>
+  
+    <div class="snowflake">
+  
+      <div class="inner">❅</div>
+  
+    </div>
+  
+    <div class="snowflake">
+  
+      <div class="inner">❅</div>
+  
+    </div>
+  
+    <div class="snowflake">
+  
+      <div class="inner">❅</div>
+  
+    </div>
+  
+    <div class="snowflake">
+  
+      <div class="inner">❅</div>
+  
+    </div>`)
+
+    document.documentElement.style.setProperty('--shit-color', `#afe4f0`)
+
+}
+
+if ((date.getMonth() == 11 && (searchParams.get("holiday") == "christmas" || !searchParams.has("holiday"))|| searchParams.get("holiday") == "christmas")) christmas()
+
 if (searchParams.has("color")) {
   let color = searchParams.get("color")
   if (color != undefined) {
@@ -361,12 +478,3 @@ $("body").keydown(function(e) {
 });
 
 tbarResize()
-
-let date = new Date()
-let currentTime = date.toLocaleString("en-US", {
-  hour12: true,
-  minute: "numeric",
-  hour: "numeric"
-})
-
-$("#time").text(currentTime)
