894391b2ae58e26e83e87dbd87c75874e9b3d5b3
BuildTools <danieldandalii@gmail.com>
2023-12-03 18:32:14 +0200

    site upgrades :3

--- stuff.js
+++ stuff.js
@@ -92,8 +92,11 @@ function dragElement(elmnt) {
 
     if (finalX > 0) {
       finalX = finalX > window.innerHeight-(elmnt.offsetHeight/4)? window.innerHeight-(elmnt.offsetHeight/4) : finalX //buggy
+      //$(document).css("filter", "brightness(1)")
+      document.documentElement.style.filter = "brightness(100%)";
     } else {
       finalX = finalX < 0? 0 : finalX
+      document.documentElement.style.filter = "brightness(40%)";
     }
 
     if (finalY > 0) {
@@ -115,6 +118,13 @@ function dragElement(elmnt) {
 
     document.onmousemove = null;
     document.ontouchmove = null;
+
+    if (finalX == 0) {
+      maximized=true
+      maximizeRefresh()
+      tbarResize()
+      maximizeRefresh()
+    }
   }
 }
 
@@ -135,6 +145,8 @@ function tbarResize() {
     $("#desccontent").css("height", `${conth*0.5}px`)
     $("#met").css("fontSize", `${conth*0.02536}px`)
     $("#desc").css("fontSize", `${conth*0.02536}px`)
+
+    $(".inactive-taskbar-element").css("border-width", `${conth*0.005}px`)
 
     var pfp = document.getElementById("pfp")
     pfp.height=conth*0.1739
@@ -165,9 +177,11 @@ var content = document.getElementById("content")
 var button = document.getElementById("maximize-button")
 
 function maximizeRefresh() {
+  document.documentElement.style.filter = "brightness(100%)";
   if (!maximized) {
     $("#windowheader").css("display", "flex")
     $("#minimize-button").removeInlineCss()
+    $("#minimize-fit-button").removeInlineCss()
     $("body").removeInlineCss()
     $("#window").removeInlineCss("border")
     $("#window").removeInlineCss("height")
@@ -185,14 +199,11 @@ function maximizeRefresh() {
     $("#taskbar").css("display", "none")
     $("#window").css("border", "none")
     $("#window").css("top", "0")
-/*     $("#window").css("left", "0") */
     $("#content").css("height", "100vh")
-/*     $("#content").css("width", "100vw") */
     $("#window").css("height", "100vh")
-    //$("#windowheader").css("position", "absolute")
-    //$("#windowheader").css("width", "100vw")
-    $("#minimize-button").css("display", "block")
     $("#minimize-button").css("width", `${Math.min(window.innerWidth, window.innerHeight)/21.4}px`)
+    $("#minimize-fit-button").css("width", `${$("#scrright").height()}px`)
+    $("#minimize-fit-button").css("height", `${$("#scrright").height()}px`)
 
     if (window.innerWidth/window.innerHeight >= 106/69) {
       $("#content").css("width", (window.innerHeight/69*106))
@@ -201,8 +212,15 @@ function maximizeRefresh() {
       $("#content").css("width", window.innerWidth)
       $("#window").css("left", 0)
     }
+
+    if (window.innerWidth/window.innerHeight >= 38/23) {
+      $("#minimize-button").css("display", "block")
+      $("#minimize-fit-button").css("display", "none")
+    } else {
+      $("#minimize-button").css("display", "none")
+      $("#minimize-fit-button").css("display", "block")
+    }
   }
-  //tbarResize()
 }
 
 async function rgb() {
@@ -251,21 +269,15 @@ function old() {
 
   $("#content").css("padding", "2%");
 
-  //$("#content").css("maxWidth", `${window.innerHeight/69*106}px`)
-
   $("#content").css("width", `100vw`)
-
-  //$("#content").css("width", `min(100vw, ${$("#content").height()/69*106}px`)
   $("#content").css("position", "absolute")
 
   $("#content").css("top", "0");
   $("#content").css("left", "0");
-  //$("#content").css("left", `${(window.innerWidth-(window.innerHeight/69*106))/2}px`)
 }
 
 if (searchParams.has("break")) throw EvalError;
 if (searchParams.has("old")) old();
-/* if (searchParams.has("rgb")) rgb(); */
 if (searchParams.has("no-tb")) $("#taskbar").remove();
 if (searchParams.has("creditless")) $("#credits").remove();
 
@@ -280,102 +292,7 @@ let currentTime = date.toLocaleString("en-US", {
 $("#time").text(currentTime)
 
 function christmas() {
-  $("body").append(`
-  <style>
-
-  /* customizable snowflake styling */
-  
-  .snowflake {
-  
-    color: #fff;
-  
-    font-size: 2em;
-  
-    font-family: Arial, sans-serif;
-  
-    text-shadow: 0 0 5px #000;
-  
-  }
-  
-   
-  
-  .snowflake,.snowflake .inner{animation-iteration-count:infinite;animation-play-state:running}@keyframes snowflakes-fall{0%{transform:translateY(0)}100%{transform:translateY(110vh)}}@keyframes snowflakes-shake{0%,100%{transform:translateX(0)}50%{transform:translateX(80px)}}.snowflake{position:fixed;top:-10%;z-index:9999;-webkit-user-select:none;user-select:none;cursor:default;animation-name:snowflakes-shake;animation-duration:3s;animation-timing-function:ease-in-out}.snowflake .inner{animation-duration:10s;animation-name:snowflakes-fall;animation-timing-function:linear}.snowflake:nth-of-type(0){left:1%;animation-delay:0s}.snowflake:nth-of-type(0) .inner{animation-delay:0s}.snowflake:first-of-type{left:10%;animation-delay:1s}.snowflake:first-of-type .inner,.snowflake:nth-of-type(8) .inner{animation-delay:1s}.snowflake:nth-of-type(2){left:20%;animation-delay:.5s}.snowflake:nth-of-type(2) .inner,.snowflake:nth-of-type(6) .inner{animation-delay:6s}.snowflake:nth-of-type(3){left:30%;animation-delay:2s}.snowflake:nth-of-type(11) .inner,.snowflake:nth-of-type(3) .inner{animation-delay:4s}.snowflake:nth-of-type(4){left:40%;animation-delay:2s}.snowflake:nth-of-type(10) .inner,.snowflake:nth-of-type(4) .inner{animation-delay:2s}.snowflake:nth-of-type(5){left:50%;animation-delay:3s}.snowflake:nth-of-type(5) .inner{animation-delay:8s}.snowflake:nth-of-type(6){left:60%;animation-delay:2s}.snowflake:nth-of-type(7){left:70%;animation-delay:1s}.snowflake:nth-of-type(7) .inner{animation-delay:2.5s}.snowflake:nth-of-type(8){left:80%;animation-delay:0s}.snowflake:nth-of-type(9){left:90%;animation-delay:1.5s}.snowflake:nth-of-type(9) .inner{animation-delay:3s}.snowflake:nth-of-type(10){left:25%;animation-delay:0s}.snowflake:nth-of-type(11){left:65%;animation-delay:2.5s}
-  
-  </style>
-  
-  <div class="snowflakes" aria-hidden="true">
-  
-    <div class="snowflake">
-  
-      <div class="inner">❅</div>
-  
-    </div>
-  
-    <div class="snowflake">
-  
-      <div class="inner">❅</div>
-  
-    </div>
-  
-    <div class="snowflake">
-  
-      <div class="inner">❅</div>
-  
-    </div>
-  
-    <div class="snowflake">
-  
-      <div class="inner">❅</div>
-  
-    </div>
-  
-    <div class="snowflake">
-  
-      <div class="inner">❅</div>
-  
-    </div>
-  
-    <div class="snowflake">
-  
-      <div class="inner">❅</div>
-  
-    </div>
-  
-    <div class="snowflake">
-  
-      <div class="inner">❅</div>
-  
-    </div>
-  
-    <div class="snowflake">
-  
-      <div class="inner">❅</div>
-  
-    </div>
-  
-    <div class="snowflake">
-  
-      <div class="inner">❅</div>
-  
-    </div>
-  
-    <div class="snowflake">
-  
-      <div class="inner">❅</div>
-  
-    </div>
-  
-    <div class="snowflake">
-  
-      <div class="inner">❅</div>
-  
-    </div>
-  
-    <div class="snowflake">
-  
-      <div class="inner">❅</div>
-  
-    </div>`)
+  $("body").append(`<style>.snowflake{color:#fff;font-size:2em;font-family:Arial,sans-serif;text-shadow:0 0 5px #000}.snowflake,.snowflake .inner{animation-iteration-count:infinite;animation-play-state:running}@keyframes snowflakes-fall{0%{transform:translateY(0)}100%{transform:translateY(110vh)}}@keyframes snowflakes-shake{0%,100%{transform:translateX(0)}50%{transform:translateX(80px)}}.snowflake{position:fixed;top:-10%;z-index:9999;-webkit-user-select:none;user-select:none;cursor:default;animation-name:snowflakes-shake;animation-duration:3s;animation-timing-function:ease-in-out}.snowflake .inner{animation-duration:10s;animation-name:snowflakes-fall;animation-timing-function:linear}.snowflake:nth-of-type(0){left:1%;animation-delay:0s}.snowflake:nth-of-type(0) .inner{animation-delay:0s}.snowflake:first-of-type{left:10%;animation-delay:1s}.snowflake:first-of-type .inner,.snowflake:nth-of-type(8) .inner{animation-delay:1s}.snowflake:nth-of-type(2){left:20%;animation-delay:.5s}.snowflake:nth-of-type(2) .inner,.snowflake:nth-of-type(6) .inner{animation-delay:6s}.snowflake:nth-of-type(3){left:30%;animation-delay:2s}.snowflake:nth-of-type(11) .inner,.snowflake:nth-of-type(3) .inner{animation-delay:4s}.snowflake:nth-of-type(4){left:40%;animation-delay:2s}.snowflake:nth-of-type(10) .inner,.snowflake:nth-of-type(4) .inner{animation-delay:2s}.snowflake:nth-of-type(5){left:50%;animation-delay:3s}.snowflake:nth-of-type(5) .inner{animation-delay:8s}.snowflake:nth-of-type(6){left:60%;animation-delay:2s}.snowflake:nth-of-type(7){left:70%;animation-delay:1s}.snowflake:nth-of-type(7) .inner{animation-delay:2.5s}.snowflake:nth-of-type(8){left:80%;animation-delay:0s}.snowflake:nth-of-type(9){left:90%;animation-delay:1.5s}.snowflake:nth-of-type(9) .inner{animation-delay:3s}.snowflake:nth-of-type(10){left:25%;animation-delay:0s}.snowflake:nth-of-type(11){left:65%;animation-delay:2.5s}</style><div class="snowflakes" aria-hidden="true"><div class="snowflake"><div class="inner">❅</div></div><div class="snowflake"><div class="inner">❅</div></div><div class="snowflake"><div class="inner">❅</div></div><div class="snowflake"><div class="inner">❅</div></div><div class="snowflake"><div class="inner">❅</div></div><div class="snowflake"><div class="inner">❅</div></div><div class="snowflake"><div class="inner">❅</div></div><div class="snowflake"><div class="inner">❅</div></div><div class="snowflake"><div class="inner">❅</div></div><div class="snowflake"><div class="inner">❅</div></div><div class="snowflake"><div class="inner">❅</div></div><div class="snowflake"><div class="inner">❅</div></div>`)
 
     document.documentElement.style.setProperty('--shit-color', `#afe4f0`)
 
@@ -420,6 +337,11 @@ $("#maximize-button").click(function() {
     tbarResize()
 })
 
+$("#minimize-fit-button").click(function() {
+  maximized = !maximized;
+  maximizeRefresh()
+})
+
 $("#minimize-button").click(function() {
   maximized = !maximized;
   maximizeRefresh()
@@ -447,6 +369,7 @@ window.mobileCheck = function() {
 function mobileAdapt() {
     if (mobileCheck()) {
         $("#minimize-button").remove();
+        $("#minimize-fit-button").remove();
         maximized=true;
         maximizeRefresh();
         tbarResize();
